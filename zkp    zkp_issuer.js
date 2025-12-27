b67a7a4dc05baa4fb9339c391f1d574461c4ef0e
sck12021997-hue <sck12021997@gmail.com>
2025-12-27 12:03:23 +0530

    Update zkp    zkp_issuer.js

--- zkp    zkp_issuer.js
+++ zkp    zkp_issuer.js
@@ -1,1 +1,13 @@
+// Simulated ZKP Issuer
+const crypto = require("crypto");
+
+function issueZKP(vci) {
+  return {
+    proof: crypto.createHash("sha256").update(vci).digest("hex"),
+    message: "Valid voter, eligible, not voted"
+  };
+}
+
+// Demo
+console.log(issueZKP("sample-vci-123"));
 
