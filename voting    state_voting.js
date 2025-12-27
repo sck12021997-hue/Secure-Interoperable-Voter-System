4cfa0a9a44254c073f516947b8909208f08e40da
sck12021997-hue <sck12021997@gmail.com>
2025-12-27 12:04:45 +0530

    Update voting    state_voting.js

--- voting    state_voting.js
+++ voting    state_voting.js
@@ -1,1 +1,15 @@
+// State Voting Portal (Token Based)
+let usedTokens = [];
+
+function vote(token) {
+  if (usedTokens.includes(token)) {
+    return "VOTE_REJECTED_ALREADY_USED";
+  }
+  usedTokens.push(token);
+  return "VOTE_CAST_SUCCESSFULLY";
+}
+
+// Demo
+console.log(vote("token123"));
+console.log(vote("token123"));
 
