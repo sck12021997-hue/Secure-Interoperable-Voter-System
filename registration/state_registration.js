cb7f06a3dae16e277ea92240891f4a77f8ed6d63
sck12021997-hue <sck12021997@gmail.com>
2025-12-27 11:56:27 +0530

    Create state_registration.js

---
+++ registration/state_registration.js
@@ -0,0 +1,17 @@
+/registration
+   state_registration.js
+
+/identity
+   crypto_identity.js
+
+/zkp
+   zkp_issuer.js
+
+/deduplication
+   national_dedup.js
+
+/mobility
+   mobility_token.js
+
+/voting
+   state_voting.js
