5756f49d1c8279c9743fc3616c1bfd6aee9973a1
sck12021997-hue <sck12021997@gmail.com>
2025-12-27 12:04:18 +0530

    Update mobility    mobility_token.js

--- mobility    mobility_token.js
+++ mobility    mobility_token.js
@@ -1,1 +1,13 @@
+// Mobility Token Generator
+const crypto = require("crypto");
 
+function generateMobilityToken(vci) {
+  return {
+    token: crypto.randomBytes(16).toString("hex"),
+    linkedVCI: vci,
+    valid: true
+  };
+}
+
+// Demo
+console.log(generateMobilityToken("sample-vci-123"));
