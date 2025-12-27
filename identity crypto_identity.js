a90d4c53a6610cd117b8fc836a9d3f4f841355ef
sck12021997-hue <sck12021997@gmail.com>
2025-12-27 12:02:57 +0530

    Update identity crypto_identity.js

--- identity crypto_identity.js
+++ identity crypto_identity.js
@@ -1,1 +1,16 @@
+// Cryptographic Identity Generator
+const crypto = require("crypto");
+
+function generateVCI(name, biometric) {
+  return crypto
+    .createHash("sha256")
+    .update(name + biometric)
+    .digest("hex");
+}
+
+// Demo
+console.log({
+  VCI: generateVCI("Ram", "fingerprint123"),
+  note: "No name/caste/religion stored"
+});
 
